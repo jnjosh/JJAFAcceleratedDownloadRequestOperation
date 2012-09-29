4ea7094645467a910195ee80a34a2a91ac800e4f
Josh Johnson <josh@twotoasters.com>
2012-09-29 01:08:16 -0400

    added sample project, stubbed out header/implementation files, added AFNetworkind submodule on 1.0RC2

---
+++ Sample/AFAcceleratedDownloadRequestOperation-Sample/JJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JJAppDelegate.h
+//  AFAcceleratedDownloadRequestOperation-Sample
+//
+//  Created by Josh Johnson on 9/29/12.
+//  Copyright (c) 2012 jnjosh.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Sample/AFAcceleratedDownloadRequestOperation-Sample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AFAcceleratedDownloadRequestOperation-Sample
+//
+//  Created by Josh Johnson on 9/29/12.
+//  Copyright (c) 2012 jnjosh.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JJAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([JJAppDelegate class]));
+	}
+}
